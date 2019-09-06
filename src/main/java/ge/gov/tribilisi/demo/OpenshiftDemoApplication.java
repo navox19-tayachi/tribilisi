b721ed129aa3c5e23c0369a5298d52cb673420ad
navox19-tayachi <nadhir.tayachi@esprit.tn>
2019-09-06 10:23:14 +0200

    Initial commit

---
+++ src/main/java/ge/gov/tribilisi/demo/OpenshiftDemoApplication.java
@@ -0,0 +1,13 @@
+package ge.gov.tribilisi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftDemoApplication.class, args);
+	}
+
+}
