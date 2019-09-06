4ff240a629145b433b69501dae4afc8bf45ff195
navox19-tayachi <nadhir.tayachi@esprit.tn>
2019-09-06 11:37:29 +0200

    adding hello world message to console

--- src/main/java/ge/gov/tribilisi/demo/OpenshiftDemoApplication.java
+++ src/main/java/ge/gov/tribilisi/demo/OpenshiftDemoApplication.java
@@ -8,6 +8,7 @@ public class OpenshiftDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenshiftDemoApplication.class, args);
+		System.out.println("Hello World !");
 	}
 
 }
